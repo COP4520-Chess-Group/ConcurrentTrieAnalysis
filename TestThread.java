4504c4360499dbd5a432bce66f92a0b346d66e94
Gregory Travis <gregorytravis711@gmail.com>
2022-03-25 17:23:48 -0400

    Switched way test sets work to using word banks

--- TestThread.java
+++ TestThread.java
@@ -7,38 +7,45 @@ public class TestThread extends Thread
             BufferedReader br = new BufferedReader(new FileReader(this.getName()));
             try{
                 int commands = Integer.parseInt(br.readLine());
-                String cmdType, str;
-                StringWrapper s;
+                int wordCount = Integer.parseInt(br.readLine());
+                String cmdType;
+                int word;
+                String wordBank[] = new String[wordCount];
+                StringWrapper words[] = new StringWrapper[wordCount];
+                for (int i = 0; i < wordCount; i++)
+                {
+                    wordBank[i] = br.readLine();
+                    words[i] = new StringWrapper(wordBank[i]);
+                }
                 for (int i = 0; i < commands; i++)
                 {
                     cmdType = br.readLine();
-                    str = br.readLine();
-                    s = new StringWrapper(str);
+                    word = Integer.parseInt(br.readLine());
                     if (cmdType.compareTo("ADD") == 0)
                     {
-                        Tester.t.add(s);
-                        System.out.println(this.getName() + " added " + str);
+                        Tester.t.add(words[word]);
+                        System.out.println(this.getName() + " added " + wordBank[word]);
                     }
                     else if (cmdType.compareTo("CONTAINS") == 0)
                     {
-                        if (Tester.t.contains(s))
+                        if (Tester.t.contains(words[word]))
                         {
-                            System.out.println(this.getName() + " found that contains " + str + " is true");
+                            System.out.println(this.getName() + " found that contains " + wordBank[word] + " is true");
                         }
                         else
                         {
-                            System.out.println(this.getName() + " found that contains " + str + " is false"); 
+                            System.out.println(this.getName() + " found that contains " + wordBank[word] + " is false"); 
                         }
                     }
                     else if (cmdType.compareTo("REMOVE") == 0)
                     {
-                        if (Tester.t.remove(s))
+                        if (Tester.t.remove(words[word]))
                         {
-                            System.out.println(this.getName() + " removed " + str);
+                            System.out.println(this.getName() + " removed " + wordBank[word]);
                         }
                         else
                         {
-                            System.out.println(this.getName() + " failed to remove " + str);
+                            System.out.println(this.getName() + " failed to remove " + wordBank[word]);
                         }
                     }
                 }
